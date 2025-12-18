a8d9dec8001a0e9e1154c9c44db35cddcc859505
HrithikgowdaD <hrithikgowdad@gmail.com>
2025-12-18 15:47:28 +0530

    WOrking on the project cards

--- main.js
+++ main.js
@@ -137,3 +137,27 @@ themeToggle.addEventListener("click", () => {
     }
 });
 
+// PROJECT IMAGE CLICK PREVIEW
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeModal = document.querySelector(".close-modal");
+
+document.querySelectorAll(".project-preview img").forEach(img => {
+    img.addEventListener("click", () => {
+        modal.style.display = "flex";
+        modalImg.src = img.src;
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close when clicking outside image
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
